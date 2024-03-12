d8c78d8c3d5c8403d145248bb9e5711bf62d17ec
Kalpit Rathore <Kalpitrathore@gmail.com>
2024-03-13 02:23:44 +0530

    feat: Added validations for querySelector

--- src/module/charts/chart.validation.ts
+++ src/module/charts/chart.validation.ts
@@ -1,8 +1,13 @@
 import Joi from 'joi';
 
 export const produceChartSchema = Joi.object({
-  chartId: Joi.string().required(),
-  querySelector: Joi.string().required(),
+  chartId: Joi.string().required(), // TODO: Find a way to automatic assign chartId
+  querySelector: Joi.string()
+    .regex(/^(\.|#).*/)
+    .required()
+    .messages({
+      'string.pattern.base': 'The string must start with either "." or "#"'
+  }),
   chartData: Joi.object({
     type: Joi.string().required(),
     data: Joi.object().required(),
